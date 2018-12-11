72365eed683fc26d89633a4bde33a736673edb1c
Hermann Gudmundsson <hemmigumm@hotmail.com>
2018-12-11 17:48:11 +0000

    price list controller framework added

---
+++ services/price_list_controller.py
@@ -0,0 +1,13 @@
+from services.controller import Controller
+from ui.menu import Menu
+
+class PriceListController(Controller):
+    def __init__(self, service, priority_controller=False):
+        super().__init__(service, priority_controller)
+        self._menu_stack.append(self.__make_price_chart_menu())
+
+    def __make_price_chart_menu(self):
+        header = "temp"
+        menu = Menu(header=header, stop_function=self.stop,
+                    back_function=self.back)
+        return menu
